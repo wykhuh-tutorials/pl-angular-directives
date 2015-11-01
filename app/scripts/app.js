5df956da04f55a08af77cb7831d2f65cc328cce1
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-01 12:33:22 -0800

    show user info in directive template

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,14 +3,21 @@
 angular
   .module('app', []);
 
-angular.module('app').controller('mainCtrl', function(){
-
+angular.module('app').controller('mainCtrl', function($scope){
+  $scope.user = {
+    name: 'Jane Doe',
+    address: {
+      street: '555 Main',
+      city: 'Springfield',
+      planet: 'Earth'
+    }
+  }
 })
 
 
 angular.module('app').directive('userInfoCard', function(){
   return {
-    template: 'User Info Card',
+    template: 'Name: {{user.name}}<br>Address:<br>{{user.address.street}}<br>{{user.address.city}}<br>{{user.address.planet}}',
     restrict: 'E'
   }
 })
