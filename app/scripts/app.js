2482b6ac43c4b703142be9bc4a9bbf32bf44d7e9
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-01 15:33:55 -0800

    add controller for knight me button; shared scope

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,23 +5,38 @@ angular
 
 angular.module('app').controller('mainCtrl', function($scope){
   this.user = {
-    name: 'Jane Doe',
+    name: 'Luke Skywalker',
     address: {
       street: '555 Main',
-      city: 'Springfield',
-      planet: 'Earth'
+      city: 'Secret Rebel Base',
+      planet: 'Yavin 4'
     },
     friends: [
-      'Jenn',
-      'Joe',
-      'Jeff'
+      'Han',
+      'Leia',
+      'C3PO'
     ]
   }
+  console.log('parent scope', $scope.$id);
 })
 
 angular.module('app').directive('plUserInfoCard', function(){
+
   return {
-    templateUrl: 'scripts/user-info-card/index.html',
-    restrict: 'E'
+    templateUrl: 'scripts/user-info-card/user-info-card.html',
+    restrict: 'E',
+    // with shared scope, directive and parent controller need different
+    // controllerAs alias
+    controllerAs: 'card',
+    controller: function($scope) {
+      this.knightMe = function (user) {
+        user.rank = 'knight';
+      }
+
+      console.log('directive scope', $scope.$id);
+    }
   }
 })
+
+
+
