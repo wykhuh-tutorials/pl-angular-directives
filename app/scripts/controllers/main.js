ea956e1e55a9af8b21f4945cc763091da2d54062
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-01 12:20:55 -0800

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularDirectivesApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the angularDirectivesApp
+ */
+angular.module('angularDirectivesApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
