9f95ccead2c08bef626d3d804ac9aa8817a6c114
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-01 12:28:18 -0800

    show static text

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -2,3 +2,15 @@
 
 angular
   .module('app', []);
+
+angular.module('app').controller('mainCtrl', function(){
+
+})
+
+
+angular.module('app').directive('userInfoCard', function(){
+  return {
+    template: 'User Info Card',
+    restrict: 'E'
+  }
+})
