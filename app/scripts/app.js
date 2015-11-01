d10391424d2e086e5b06aedcb23d7c058975cbc9
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-01 12:41:14 -0800

    display an array of friends

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -10,14 +10,19 @@ angular.module('app').controller('mainCtrl', function($scope){
       street: '555 Main',
       city: 'Springfield',
       planet: 'Earth'
-    }
+    },
+    friends: [
+      'Jenn',
+      'Joe',
+      'Jeff'
+    ]
   }
 })
 
 
 angular.module('app').directive('userInfoCard', function(){
   return {
-    template: 'Name: {{user.name}}<br>Address:<br>{{user.address.street}}<br>{{user.address.city}}<br>{{user.address.planet}}',
+    template: 'Name: {{user.name}}<br><div ng-show="!!user.address">Address:<br>{{user.address.street}}<br>{{user.address.city}}<br>{{user.address.planet}}</div> <div>Friends: <div ng-repeat="friend in user.friends">{{friend}}</div></div>',
     restrict: 'E'
   }
 })
