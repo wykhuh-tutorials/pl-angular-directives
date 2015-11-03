13ab7b94233f2e5c19750dc3c0e9dd485c8c95c9
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-02 23:55:00 -0800

    add directive to stop/start video using spacebar

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,8 +7,25 @@ angular.module('app').controller('mainCtrl', function($scope){
 
 })
 
-angular.module('app').directive('plDemo', function(){
+angular.module('app').directive('spacebarSupport', function(){
   return {
-    template: 'Demo'
+    attribute: 'A',
+    // use link to modify the html on the page.
+    link: function(scope, el, attrs ) {
+      // add keypress event listener on the body
+      $('body').on('keypress', function(evt){
+        // el is wrapped in jquery wrapper. use el[0] to get to DOM element
+        var vidEl = el[0]
+        // check if spacebar is pressed
+        if (evt.keyCode === 32) {
+          // paused and play() are methods on html5 <video>
+          if(vidEl.paused) {
+            vidEl.play();
+          } else {
+            vidEl.pause();
+          }
+        }
+      })
+    }
   }
 })
