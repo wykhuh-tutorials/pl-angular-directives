11cd517a454229b25ccf28652010c8f38ad58e74
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-05 06:43:38 -0800

    add my-click directive

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,11 +4,27 @@ angular
   .module('app', []);
 
 angular.module('app').controller('mainCtrl', function($scope){
+  $scope.data = {message: 'I have not been clicked'};
+  $scope.clickHandler = function(p) {
+    p.message = 'I have been clicked';
+  };
+});
 
-})
-
-angular.module('app').directive('demo', function(){
+angular.module('app').directive('myClick', function($parse){
   return {
-    template: 'Demo'
-  }
-})
+    // use link instead of isolate scope for decorator directives
+    // because isolate scope can break other directives
+    link: function(scope, el, attrs) {
+      // my-click="clickHandler(data)"
+      // $parse - angular looks for clickHandler function
+      var fn = $parse(attrs.myClick);
+      el.on('click', function() {
+        // use $apply because click occurs outside the digest cycle.
+        // $apply starts new digest cycle
+        scope.$apply(function() {
+          fn(scope);
+        });
+      });
+    },
+  };
+});
