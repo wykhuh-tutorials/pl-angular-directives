8fe22b2750e095ededded7fb93454a40d0a4c326
Wai-Yin Kwan <waiyin@gophilosophie.com>
2015-11-08 09:45:29 -0800

    use watch to change font size

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,11 +4,23 @@ angular
   .module('app', []);
 
 angular.module('app').controller('mainCtrl', function($scope){
-
+  $scope.size = 150;
 })
 
-angular.module('app').directive('demo', function(){
+angular.module('app').directive('fontScale', function(){
   return {
-    template: 'Demo'
+    restrict: 'A',
+    link: function(scope, el, attrs) {
+      // watch runs on page load and when value changes
+
+      // $watch(thing to watch, function to execute when value changes)
+
+      // use attrs['fontScale'] instead of size directly to avoid binding
+      // directive to controller
+      scope.$watch(attrs['fontScale'], function(newValue, oldValue){
+        // el is jquery element so we can use jquery to change css
+        el.css('font-size', newValue + '%');
+      })
+    }
   }
 })
